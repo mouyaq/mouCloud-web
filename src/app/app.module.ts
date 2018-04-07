ef339831db6e289c7784b6ce12450b1115e24e54
mouyaq <mouyaq@gmail.com>
2018-04-07 04:57:18 +0200

    feat: session service authentication methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import './rxjs.operators';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/misc/navbar/navbar.component';
--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,8 +1,41 @@
+import { Observable, Subject } from 'rxjs/Rx';
+import { User } from './../model/user.model';
+import { BaseApiService } from './base-api.service';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+const CURRENT_USER_KEY = 'currentUser';
 
 @Injectable()
-export class SessionService {
+export class SessionService extends BaseApiService {
+  protected static readonly SESSION_API = `${BaseApiService.BASE_API}/session`;
 
-  constructor() { }
+  private user: User;
+  private userSubject: Subject<User> = new Subject();
+
+  constructor(private http: Http) {
+    super();
+    this.user = JSON.parse(localStorage.getItem(CURRENT_USER_KEY));
+    this.notifyUserChanges();
+  }
+
+  authenticate(user: User): Observable<User> {
+    return this.http.post(SessionService.SESSION_API, JSON.stringify(user), BaseApiService.defaultOptions)
+      .map(res => {
+        return this.doAuthentication(res.json());
+      })
+      .catch(error => this.handleError(error));
+  }
+
+  private doAuthentication(user: User): User {
+    this.user = user;
+    localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(this.user));
+    this.notifyUserChanges();
+    return this.user;
+  }
+
+  private notifyUserChanges() {
+    this.userSubject.next(this.user);
+  }
 
 }
