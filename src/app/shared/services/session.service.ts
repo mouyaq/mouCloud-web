b2d3ab0b343edda2d15ecd32236726ddd2580b28
mouyaq <mouyaq@gmail.com>
2018-04-10 02:04:59 +0200

    feat: change localStorage to sessionStorage

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -2,7 +2,7 @@ import { Observable, Subject } from 'rxjs/Rx';
 import { User } from './../model/user.model';
 import { BaseApiService } from './base-api.service';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 const CURRENT_USER_KEY = 'currentUser';
 
@@ -15,7 +15,7 @@ export class SessionService extends BaseApiService {
 
   constructor(private http: Http) {
     super();
-    this.user = JSON.parse(localStorage.getItem(CURRENT_USER_KEY));
+    this.user = JSON.parse(sessionStorage.getItem(CURRENT_USER_KEY));
     this.notifyUserChanges();
   }
 
@@ -29,8 +29,8 @@ export class SessionService extends BaseApiService {
 
   private doAuthentication(user: User): User {
     this.user = user;
-    BaseApiService.defaultOptions.headers.append('Set-Cookie', this.user.cookie);
-    localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(this.user));
+    //BaseApiService.defaultOptions.headers.append('Set-Cookie', this.user.cookie);
+    sessionStorage.setItem(CURRENT_USER_KEY, JSON.stringify(this.user));
     this.notifyUserChanges();
     return this.user;
   }
@@ -48,7 +48,15 @@ export class SessionService extends BaseApiService {
   }
 
   logout(): Observable<void> {
+    // let headers = new Headers();
+    // headers.append('content-type', 'application/json');
+    // headers.append('vmware-api-session-id', this.user.token);
+    // let opts = new RequestOptions({withCredentials: true});
+    // opts.headers = headers;
+    BaseApiService.defaultOptions.headers.append('vmware-api-session-id', this.user.token);
+    // console.log(BaseApiService.defaultOptions);
     return this.http.delete(SessionService.SESSION_API, BaseApiService.defaultOptions)
+    // return this.http.delete(SessionService.SESSION_API, opts)
       .map(res => {
         return this.doLogout();
       })
@@ -56,9 +64,9 @@ export class SessionService extends BaseApiService {
   }
 
   protected doLogout(): void {
+    // BaseApiService.defaultOptions.headers.delete('Set-Cookie');
+    sessionStorage.removeItem(CURRENT_USER_KEY);
     this.user = null;
-    BaseApiService.defaultOptions.headers.delete('Set-Cookie');
-    localStorage.removeItem(CURRENT_USER_KEY);
     this.notifyUserChanges();
   }
 
